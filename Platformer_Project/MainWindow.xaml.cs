5100a1512c3e64bb574dc86f52fb54be1df9940f
Davis Johnson <davbjohn@ut.utm.edu>
2022-03-31 20:32:46 -0500

    Gravity cool

--- Platformer_Project/MainWindow.xaml.cs
+++ Platformer_Project/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Platformer_Project
 {
@@ -20,20 +21,112 @@ namespace Platformer_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer dtClockTime = new DispatcherTimer();
+        bool goleft = false;
+        bool goright = false;
+        bool jumping = false;
+
+        int jumpSpeed = 10;
+        int force = 8;
+        int score = 0;
+
+        Rect playerHitBox;
+        Rect groundHitBox;
 
         public MainWindow()
         {
             InitializeComponent();
+            MyCanvas.Focus();
+            
+            dtClockTime.Tick += dtClockTime_Tick;
+            dtClockTime.Interval = TimeSpan.FromMilliseconds(20); //in Hour, Minutes, Second.
+            
+
+            dtClockTime.Start();
         }
 
-        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        private void keyisdown(object sender, KeyEventArgs e)
         {
             switch (e.Key)
             {
                 case Key.Left:
+                    goleft = true;
                     break;
                 case Key.Right:
+                    goright = true;
                     break;
+                
+            }
+            
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    goleft = false;
+                    break;
+                case Key.Right:
+                    goright = false;
+                    break;
+                case Key.Space:
+                    jumping = true;
+                    force = 15;
+                    jumpSpeed = -12;
+                    break;
+            }
+            if(e.Key == Key.Space && jumping == false)
+            {
+                jumping = true;
+                force = 15;
+                jumpSpeed = -12;
+                //Canvas.SetTop(Player, Canvas.GetTop(Player) - jumpSpeed);
+            }
+        }
+        private void dtClockTime_Tick(object sender, EventArgs e)
+        {
+            double x = Canvas.GetLeft(Player);
+            double y = Canvas.GetTop(Player);
+
+            Canvas.SetTop(Player, y + jumpSpeed);
+
+            
+
+            //if (goleft)
+            //{
+            //    Canvas.SetLeft(Player, x - 5);
+            //}
+
+            //if (goright)
+            //{
+            //    Canvas.SetLeft(Player, x + 5);
+            //}
+
+            playerHitBox = new Rect(x, y, Player.Width - 15, Player.Height);
+            groundHitBox = new Rect(Canvas.GetLeft(ground), Canvas.GetTop(ground), ground.Width - 15, ground.Height - 10);
+
+            if (playerHitBox.IntersectsWith(groundHitBox))
+            {
+                jumpSpeed = 0;
+                Canvas.SetTop(Player, Canvas.GetTop(ground) - Player.Height);
+                jumping = false;
+            }
+
+            if(jumping == true)
+            {
+                jumpSpeed = -9;
+
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            if(force < 0)
+            {
+                jumping = false;
             }
         }
     }
